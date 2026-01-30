7210b9cb64255934356cb00763c3518fd3c318b8
Mia Mister <MiaMister663@gmail.com>
2026-01-30 12:49:43 +0000

    Update MyEngineSystem.cpp

--- src/engine/custom/MyEngineSystem.cpp
+++ src/engine/custom/MyEngineSystem.cpp
@@ -1,1 +1,86 @@
 #include "MyEngineSystem.h"
+using namespace std;
+
+//MyEngineSystem
+MyEngineSystem::MyEngineSystem() {}
+
+SDL_Rect MyEngineSystem::getCurrentFrameRect(string id)
+{
+    SDL_Rect* rect = nullptr;
+    for (AnimatedSprite animation : loadedAnimations) {
+        if (animation.id == id)
+        {
+            rect = &animation.getCurrentRect();
+            break;
+        }
+    }
+
+    if (rect != nullptr)
+    {
+        return *rect;
+    }
+    else
+    {
+        debug("No animation found");
+        return SDL_Rect{0,0,0,0};
+    }
+}
+
+void MyEngineSystem::updateAnimations()
+{
+    for (AnimatedSprite& animation : loadedAnimations) {
+
+        if (animation.updateCooldown > 60 / animation.getFramerate())
+        {
+            animation.updateFrame();
+            animation.updateCooldown = 0;
+        }
+        else animation.updateCooldown++;
+    }
+}
+
+AnimatedSprite MyEngineSystem::createAnimation(string id, int width, int height, float frameRate, int frameCount)
+{
+    AnimatedSprite* sprite = new AnimatedSprite(id, width, height, frameRate, frameCount);
+
+    loadedAnimations.push_back(*sprite);
+
+    return *sprite;
+}
+
+//AnimatedSprite
+AnimatedSprite::AnimatedSprite(string givenid, int givenWidth, int givenHeight, float givenFrameRate, int givenFrameCount)
+{
+    id = givenid;
+    width = givenWidth;
+    height = givenHeight;
+    frameRate = givenFrameRate;
+    frameCount = givenFrameCount;
+
+    updateCooldown = 0;
+
+    generateFrames();
+}
+
+void AnimatedSprite::generateFrames()
+{
+    for (int i = 0; i < frameCount; i++) {
+        frames.push_back(SDL_Rect{
+            width * i,
+            0,
+            width,
+            height
+            });
+    }
+
+    frame = 0;
+    currentSprite = frames[0];
+}
+
+void AnimatedSprite::updateFrame()
+{
+    currentSprite = frames[frame];
+    frame++;
+
+    if (frame > frameCount - 1) frame = 0;
+}
